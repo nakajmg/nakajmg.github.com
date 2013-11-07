37183fbc335746e48689e1a30422a04a4a9a5e4d
nakajmg <nakajima.jmg@gmail.com>
2013-11-08 03:50:30 +0900

    update and add grunt

---
+++ jekyll/_site/Gruntfile.js
@@ -0,0 +1,103 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    copy: {
+        publish: {
+            files: [{
+                src: ['_site/js/**','_site/css/**','_site/blog/**','_site/img/**','_site/index.html'], dest: '../'
+            }]
+        },
+        dev: {
+          files: [
+              { src: ['css/**'],dest: './_site/' },
+              { src: ['js/**'],dest: './_site/' }
+            ]
+        },
+    },
+    growl: {
+        compass: {
+          message: 'compass compile',
+          title: 'compass'
+        },
+        reload: {
+          message: 'filechange',
+          title: 'livereload'
+        },
+        publish: {
+          message: 'publish finish',
+          title: 'grunt publish'
+        }
+    },
+    compass: {
+        dist: {
+            options: {
+                environment: 'development'
+            }
+        },
+        prod: {
+            options: {
+                environment: 'production'
+            }
+        }
+    },
+    connect: {
+      server: {
+        options: {
+          port: 8080,
+          base: './_site/',
+          open: 'http://localhost:8080/'
+        }
+      }
+    },
+    // shell: {
+    //   jekyll: {
+    //     command: 'jekyll serve --watch &'
+    //   }
+    // },
+    // csso: {
+    //   publish: {
+    //     files: {
+    //       './_site/css/common.min.css': ['./_site/css/common.css']
+    //     }
+    //   }
+    // },
+    watch: {
+        dist: {
+            files: ['sass/*.scss'],
+            tasks: ['compass:dist'],
+            options: {
+                livereload: false
+            }
+        },
+        // jekyllFile: {
+        //     files: ['_site/blog/**','_site/css/**','_site/js/**','_site/*.html','_site/img/**'],
+        //     // tasks: [''],
+        //     options: {
+        //       livereload: true
+        //     }
+        // }
+        copy_dev: {
+            files: ['css/*.css'],
+            tasks: ['copy:dev','growl:reload'],
+            options: {
+              livereload: true
+            }
+        }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-compass');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-connect');
+  grunt.loadNpmTasks('grunt-csso');
+  grunt.loadNpmTasks('grunt-shell');
+  grunt.loadNpmTasks('grunt-growl');
+
+  grunt.registerTask('default',['connect','watch']);
+  grunt.registerTask('develop',['compass']);
+  grunt.registerTask('publish',['csso:publish','copy:publish','growl:publish']);
+
+};
